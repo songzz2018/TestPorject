aec2349504ed825dd3f6f27be817e7a78e5389a6
eclips_user <eclips_email@atguigu.com>
2018-08-06 09:42:19 +0800

    first commit in eclips

---
+++ src/main/java/com/atguigu/git/Apple.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+public class Apple {
+
+	private String color;
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+}
